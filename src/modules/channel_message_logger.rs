648eb849e828294b34c57c78258cee02fac230d4
Smallsan <126304322+Smallsan@users.noreply.github.com>
2023-10-16 12:52:56 +0800

    Almost finished with the channel_message_logger

--- src/modules/channel_message_logger.rs
+++ src/modules/channel_message_logger.rs
@@ -7,26 +7,33 @@ use serenity::utils::Colour;
 
 
 pub async fn log_message(ctx: &Context, msg: &Message) {
-    let hash_author = HashMap::from([
-        ("icon_url", Value::String(msg.author.avatar_url().unwrap_or_default())),
-        ("name", Value::String(msg.author.name.to_string())),
-    ]);
-    let author = CreateEmbedAuthor(hash_author);
     if !msg.author.bot {
+
+        let hash_author = HashMap::from([
+            ("icon_url", Value::String(msg.author.avatar_url().unwrap_or_default())),
+            ("name", Value::String(msg.author.name.to_string())),
+        ]);
+
+        let author = CreateEmbedAuthor(hash_author);
+        
+        let message_content: String = msg.content.to_string();
+        let message_guild_id: String = msg.guild_id.unwrap_or_default().to_string();
+        let message_channel_id: u64 = *msg.channel_id.as_u64();
+        let message_channel_name: String = ctx.http.get_channel(message_channel_id).await.unwrap().to_string();
+        let message_id: String = msg.id.to_string();
+        let message_link: String = format!("[**Jump To Message**](https://discord.com/channels/{}/{}/{})",message_guild_id,message_channel_id,message_id);
+        let message_channel_link: String = format!("{}",message_channel_name);
+        let formatted_description: String = format!("** Message sent in ** {}\n{}\n{}",message_channel_link,message_link,message_content);
+
         let _msg = msg
                 .channel_id
                 .send_message(&ctx.http, |m| {
-                    m.content(&msg.content)
+                    m
                         .embed(|e| {
                             e.set_author(author)
                             .colour(Colour::MAGENTA)
-                                .description("This is a description")
+                                .description(formatted_description)
                                 .thumbnail(&msg.author.avatar_url().unwrap_or_default())
-                                .fields(vec![
-                                    ("This is the first field", "This is a field body", true),
-                                    ("This is the second field", "Both fields are inline", true),
-                                ])
-                                .field("This is the third field", "This is not an inline field", false)
                                 .footer(|f| f.text("User ID: ".to_string() + &msg.author.id.to_string() ))
                                 .timestamp(msg.timestamp)
                         })
