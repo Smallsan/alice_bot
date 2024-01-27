b15319c58065139b424beaddb9d207d88565c037
Smallsan <minec323@gmail.com>
2024-01-27 10:45:10 +0800

    Refactored log_embed_formatter

--- src/modules/formatters/log_embed_formatter.rs
+++ src/modules/formatters/log_embed_formatter.rs
@@ -5,43 +5,36 @@ use serenity::{client::Context, utils::Colour};
 use std::collections::HashMap;
 
 pub async fn log_embed_formatter(ctx: &Context, msg: &Message) -> Vec<CreateEmbed> {
-    let mut create_embed_vec: Vec<CreateEmbed> = vec![];
+    let mut create_embed_vec: Vec<CreateEmbed> = Vec::new();
 
     let hash_author = HashMap::from([
         (
             "icon_url",
             Value::String(msg.author.avatar_url().unwrap_or_default()),
         ),
-        ("name", Value::String(msg.author.name.to_string())),
+        ("name", Value::String(msg.author.name.to_owned())),
     ]);
-
-    let attachment_vec: &Vec<Attachment> = &msg.attachments;
 
     let author = CreateEmbedAuthor(hash_author);
 
-    let content: String = msg.content.to_string();
+    let content = msg.content.to_owned();
 
-    let guild_id: String = msg.guild_id.unwrap_or_default().to_string();
+    let guild_id = msg.guild_id.map_or_else(|| "Unknown-Guild".to_owned(), |id| id.to_string());
 
-    let channel_id: String = msg.channel_id.to_string();
+    let channel_id = msg.channel_id.to_string();
 
-    let channel_name = match msg.channel(&ctx.http).await {
-        Ok(channel) => channel.to_string(),
-        Err(_) => "Unknown-Channel".to_string(),
-    };
+    let channel_name = msg.channel(&ctx.http).await.map_or_else(|_| "Unknown-Channel".to_owned(), |channel| channel.to_string());
 
-    let id: String = msg.id.to_string();
+    let id = msg.id.to_string();
 
-    let link: String = format!(
+    let link = format!(
         "[**Jump To Message**](https://discord.com/channels/{}/{}/{})",
         guild_id, channel_id, id
     );
 
-    let msg_channel_link: String = format!("{}", channel_name);
-
-    let formatted_desc: String = format!(
+    let formatted_desc = format!(
         "** Message sent in ** {}\n{}\n{}",
-        msg_channel_link, link, content
+        channel_name, link, content
     );
 
     let mut message_embed = CreateEmbed::default();
@@ -51,27 +44,23 @@ pub async fn log_embed_formatter(ctx: &Context, msg: &Message) -> Vec<CreateEmbe
         .colour(Colour::MAGENTA)
         .description(formatted_desc)
         .thumbnail(msg.author.avatar_url().unwrap_or_default())
-        .footer(|footer| footer.text("User ID: ".to_string() + &msg.author.id.to_string()))
+        .footer(|footer| footer.text(format!("User ID: {}", msg.author.id)))
         .timestamp(msg.timestamp);
 
-    let mut attachment_counter = 0;
-
-    for attachment in attachment_vec {
-        attachment_counter += 1;
-        if attachment_counter == 1 {
+    for (i, attachment) in msg.attachments.iter().enumerate() {
+        if i == 0 {
             message_embed.image(&attachment.url);
-            continue;
+        } else {
+            let mut image_embed = CreateEmbed::default();
+            image_embed
+                .title(&attachment.filename)
+                .image(&attachment.url)
+                .colour(Colour::MAGENTA);
+            create_embed_vec.push(image_embed);
         }
-
-        let mut image_embed = CreateEmbed::default();
-        image_embed
-            .title(&attachment.filename)
-            .image(&attachment.url)
-            .colour(Colour::MAGENTA);
-        create_embed_vec.push(image_embed);
     }
 
-    create_embed_vec.insert(0, message_embed);
+    create_embed_vec.push(message_embed);
 
-    return create_embed_vec;
+    create_embed_vec
 }
